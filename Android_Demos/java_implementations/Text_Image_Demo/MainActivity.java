08c5a7f38a3f88cda46f4951fe3e7682b77ffe50
Rajat Bapuri <rajatbapuri@gmail.com>
2019-08-14 19:56:27 -0400

    added text image demo

---
+++ Android_Demos/java_implementations/Text_Image_Demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.textimage;
+
+/**
+ * Import the packages.
+ * Usually auto-imported by Android studio
+ * */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * This file needs no modification for this demo
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
